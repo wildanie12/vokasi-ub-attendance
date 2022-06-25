0f8734876e2e3a832b71c534ef1df759c2b2ed6d
Badar Wildanie <wildanie12@gmail.com>
2022-06-25 11:49:19 +0700

    add: models

--- app/Models/Absensi.php
+++ app/Models/Absensi.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Karyawan extends Model
+class Absensi extends Model
 {
     use HasFactory;
-
-    protected $table = "karyawan";
 }
--- app/Models/Shift.php
+++ app/Models/Shift.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Karyawan extends Model
+class Shift extends Model
 {
     use HasFactory;
-
-    protected $table = "karyawan";
 }
