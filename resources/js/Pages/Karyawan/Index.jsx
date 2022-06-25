e84e532fb46c377abaee1eff4fc05a03d5624a02
Badar Wildanie <wildanie12@gmail.com>
2022-06-25 13:09:26 +0700

    add: time profile header section

--- resources/js/Pages/Karyawan/Index.jsx
+++ resources/js/Pages/Karyawan/Index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import DateTimeMain from "../../Components/Organisms/Karyawan/DateTimeMain";
+import ProfileHeader from "../../Components/Organisms/Karyawan/ProfileHeader";
 
 const Index = () => {
     return (
-        <div className="h-screen w-full">
+        <div className="h-screen w-full flex items-center justify-center">
             <div className="container">
-                <h1 className="text-4xl">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Optio, ut.
-                </h1>
+                <div className="w-full flex divide-x items-center divide-gray-400">
+                    <DateTimeMain className="w-1/2 pr-12" />
+                    <ProfileHeader className="w-1/2 pl-12" />
+                </div>
             </div>
         </div>
     );
