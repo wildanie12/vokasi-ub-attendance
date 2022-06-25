204f264c200a564a3fdc1d1842ef75d77bfa6daf
Badar Wildanie <wildanie12@gmail.com>
2022-06-25 13:47:55 +0700

    add: timeline to the page

--- resources/js/Pages/Karyawan/Index.jsx
+++ resources/js/Pages/Karyawan/Index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import DateTimeMain from "../../Components/Organisms/Karyawan/DateTimeMain";
 import ProfileHeader from "../../Components/Organisms/Karyawan/ProfileHeader";
+import Timeline from "../../Components/Organisms/Karyawan/Timeline";
 
 const Index = () => {
     return (
-        <div className="h-screen w-full flex items-center justify-center">
-            <div className="container">
-                <div className="w-full flex divide-x items-center divide-gray-400">
-                    <DateTimeMain className="w-1/2 pr-12" />
-                    <ProfileHeader className="w-1/2 pl-12" />
+        <div className="h-screen w-full flex items-center">
+            <div className="w-full flex flex-col items-center justify-center">
+                <div className="container">
+                    <div className="w-full flex divide-x items-center divide-gray-400">
+                        <DateTimeMain className="w-1/2 pr-12" />
+                        <ProfileHeader className="w-1/2 pl-12" />
+                    </div>
+                    <h4 className="font-poppins text-center text-4xl font-light py-16 text-primary-500">
+                        Selamat Sore !
+                    </h4>
                 </div>
+                <Timeline />
             </div>
         </div>
     );
