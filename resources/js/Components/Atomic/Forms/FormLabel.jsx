a92141fa3ab6bf18d5baef5acc5e02abc7e92b1e
Badar Wildanie <wildanie12@gmail.com>
2022-06-26 13:51:59 +0700

    add: atomic component form label

---
+++ resources/js/Components/Atomic/Forms/FormLabel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { FaAtom } from "react-icons/fa";
+
+const FormLabel = ({
+    icon = <FaAtom className="w-4 h-4 text-white" />,
+    label = "Some label",
+    className = "",
+}) => {
+    return (
+        <div className={`${className} flex items-center space-x-2`}>
+            {icon}
+            <label className="uppercase text-sm text-gray-500 font-bold tracking-wider">
+                {label}
+            </label>
+        </div>
+    );
+};
+
+export default FormLabel;
