39d163fc74aa117ee0eed83675b13500024e6c9f
Badar Wildanie <wildanie12@gmail.com>
2022-07-16 08:53:02 +0700

    fix: app serverside routing adapter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @routes
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
   </head>
